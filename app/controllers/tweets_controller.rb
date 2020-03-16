5f05737a4e136954580b54ddd0093a8e762bb692
marinon0428 <61176864+marinon0428@users.noreply.github.com>
2020-03-16 16:24:10 +0900

    リンク作成

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,6 @@
+class TweetsController < ApplicationController
+  def index
+    @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'posts', to: 'posts#index'
+  get 'tweets', to: 'tweets#index'
   root 'groups#index'
-  root 'messages#index'
   resource :user, only: [:index, :edit, :update]
+  resources :posts, only: [:new, :create, :show, :edit]
+  resources :tweets, only: [:new, :create, :show, :edit]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
